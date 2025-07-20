ed92604c564ee22067446fc244a2a9ad93648265
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2025-07-20 20:33:49 +0600

    initial design

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -7,16 +8,80 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello World!"),
+        title: Text("Sample LogIn Page"),
         centerTitle: true,
-        backgroundColor: Colors.orange,
+        backgroundColor: Colors.indigo,
       ),
 
-      body: Column(
-        children: [
-        ],
-      )
-
-    );
+      body: Container(
+        padding: EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // vertical centering
+          crossAxisAlignment: CrossAxisAlignment.center, // horizontal centering
+          children: [
+            Text("Enter Your Phone Number and Password for LogIn", 
+            style: GoogleFonts.poppins(fontSize: 12,fontWeight: FontWeight.bold, color: Colors.indigo),),
+            SizedBox(
+            height: 25,
+          ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Phone Number",
+                  labelText: "Phone Number",
+                  prefixIcon: Icon(Icons.phone),
+                  suffixIcon: Icon(Icons.check),
+                  hintStyle: TextStyle(
+                    color: Colors.indigoAccent,
+                    fontSize: 14,
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.indigoAccent,
+                    fontSize: 16,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Enter Password",
+                labelText: "Password",
+                prefixIcon: Icon(Icons.lock),
+                suffixIcon: Icon(Icons.remove_red_eye),
+                hintStyle: TextStyle(
+                  color: Colors.indigoAccent,
+                  fontSize: 14,
+                ),
+                labelStyle: TextStyle(
+                  color: Colors.indigoAccent,
+                  fontSize: 16,
+                ),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+              ),
+            ),
+          SizedBox(
+            height: 20,
+          ),
+          OutlinedButton(
+            onPressed: (){},
+            child: Text('Submit', style: TextStyle(color: Colors.black),), 
+            style: OutlinedButton.styleFrom(
+              backgroundColor: Colors.indigoAccent,
+            ),
+            ),
+          ],
+          ),
+      ),
+      );
   }
 }
